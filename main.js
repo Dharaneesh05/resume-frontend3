49581d42f328e7d8c06c02d5259830462c0add55
Dharaneesh <dharaneeshcs05@gmail.com>
2025-07-13 10:30:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const BACKEND_BASE_URL = 'https://your-flask-backend.onrender.com';
+const BACKEND_BASE_URL = 'https://resume-backend-1-3ypj.onrender.com';
+
 
 function login() {
     const email = document.getElementById('admin_email').value;
     const password = document.getElementById('admin_password').value;
-    fetch('/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
-    })
+    // fetch('/login', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    //     body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+    // })
+    fetch(`${BACKEND_BASE_URL}/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+})
+
     .then(response => response.json())
     .then(data => {
         if (data.status === 'success') {
@@ -19,9 +26,11 @@ function login() {
 }
 
 function logout() {
-    fetch('/logout', {
-        method: 'POST'
-    })
+    // fetch('/logout', {
+    //     method: 'POST'
+    // })
+    fetch(`${BACKEND_BASE_URL}/logout`, { method: 'POST' })
+
     .then(response => response.json())
     .then(data => {
         if (data.status === 'success') {
